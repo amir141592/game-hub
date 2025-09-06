7e027c58cf9c20229dd05da4c67ec2f91c3fb0e4
amir141592 <amir.allahdadian@gmail.com>
2025-09-07 00:15:06 +0330

    feat: add Material-UI and Emotion for styling, implement custom theme
    - Added dependencies for @emotion/react, @emotion/styled, @fontsource/roboto, and @mui/material in package.json.
    - Wrapped the App component with ThemeProvider to apply a custom Material-UI theme in main.tsx.
    - Imported Roboto font styles for different font weights.

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,40 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./App.css";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+
 import App from "./App.tsx";
+
+import "./App.css";
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+
+const theme = createTheme({
+	palette: {
+		primary: {
+			light: "#f9ed3f",
+			main: "#fddb00",
+			dark: "#fea900",
+			contrastText: "#000",
+		},
+		secondary: {
+			light: "#626262",
+			main: "#222222",
+			contrastText: "#fff",
+		},
+		info: {
+			light: "#84b9ff",
+			main: "#699bfe",
+			dark: "#5f79da",
+		},
+	},
+});
 
 createRoot(document.getElementById("root")!).render(
 	<StrictMode>
-		<App />
+		<ThemeProvider theme={theme}>
+			<App />
+		</ThemeProvider>
 	</StrictMode>
 );
